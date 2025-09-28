b8fb1556dbeada1ff0bfc97d9baa7109b04b96fd
Đặng Quang Hiển <nguyenha280876@gmail.com>
2025-09-28 23:04:33 +0700

    Fix voucher email templates to use exposed data

--- templates/emails/plain/rewardx-voucher.php
+++ templates/emails/plain/rewardx-voucher.php
@@ -7,9 +7,9 @@ if (!defined('ABSPATH')) {
 
 <?php esc_html_e('Cảm ơn bạn đã đổi thưởng trên cửa hàng của chúng tôi. Dưới đây là thông tin voucher:', 'woo-rewardx-lite'); ?>
 
-<?php esc_html_e('Mã voucher:', 'woo-rewardx-lite'); ?> <?php echo $email->data['coupon_code']; ?>
-<?php esc_html_e('Giá trị:', 'woo-rewardx-lite'); ?> <?php echo wp_strip_all_tags(wc_price($email->data['coupon_amount'])); ?>
-<?php esc_html_e('Hết hạn vào:', 'woo-rewardx-lite'); ?> <?php echo $email->data['coupon_expiry']; ?>
+<?php esc_html_e('Mã voucher:', 'woo-rewardx-lite'); ?> <?php echo $coupon_code; ?>
+<?php esc_html_e('Giá trị:', 'woo-rewardx-lite'); ?> <?php echo wp_strip_all_tags(wc_price($coupon_amount)); ?>
+<?php esc_html_e('Hết hạn vào:', 'woo-rewardx-lite'); ?> <?php echo $coupon_expiry; ?>
 
 <?php esc_html_e('Voucher chỉ sử dụng cho tài khoản email này và có hiệu lực một lần.', 'woo-rewardx-lite'); ?>
 
